fc8ba05b429704febb3ed9b615835bda7eba70f8
Nai Hamdan <hamdannai16@gmail.com>
2026-05-17 13:54:32 +0300

    added history counter screen skeleton

---
+++ lib/screens/history_counter_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HistoryCounterScreen extends StatefulWidget {
+  const HistoryCounterScreen({super.key});
+  @override
+  State<HistoryCounterScreen> createState() => _HistoryCounterScreenState();
+}
+
+class _HistoryCounterScreenState extends State<HistoryCounterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
